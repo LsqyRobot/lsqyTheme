6150f1f203d0d232ac999b0b238b9f0d856d246f
LsqyRobot <lsqyRobot@gmail.com>
2023-08-10 11:28:58 +0800

    最新的主题信息

--- source/js/dytitle.js
+++ source/js/dytitle.js
@@ -2,12 +2,12 @@ var OriginTitile = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="shortcut icon"]').attr('href', "/img/favicon.png");
+        $('[rel="shortcut icon"]').attr('href', "/img/favicon.ico");
         document.title = '没想到吧！！！';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="shortcut icon"]').attr('href', "/img/favicon.png");
+        $('[rel="shortcut icon"]').attr('href', "/img/favicon.ico");
         document.title = '开个玩笑，哈哈哈！！！ ' + OriginTitile;
         titleTime = setTimeout(function () {
             document.title = OriginTitile;
